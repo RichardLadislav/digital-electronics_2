deddf8ca38f323c9800ebe61dac66a204738c915
davidhro <david.hromas.00@gmail.com>
2022-11-09 14:46:57 +0100

    comm

--- lab7-i2c/src/main.c
+++ lab7-i2c/src/main.c
@@ -45,7 +45,7 @@ int main(void)
 
     // Configure 16-bit Timer/Counter1 to test one I2C address
     // Set prescaler to 33 ms and enable interrupt
-    TIM1_overflow_33ms();
+    TIM1_overflow_4s();
     TIM1_overflow_interrupt_enable();
 
     // Enables interrupts by setting the global interrupt mask
@@ -73,14 +73,159 @@ int main(void)
  **********************************************************************/
 ISR(TIMER1_OVF_vect)
 {
-    static uint8_t sla = 8;  // I2C Slave address
+    static uint8_t sla = 92;  // I2C Slave address
     uint8_t ack;             // ACK response from Slave
     char string[3];          // String for converting numbers by itoa()
+    
+    struct Air_parameters_structure {
+    uint8_t humid_int;
+    uint8_t humid_dec;
+    uint8_t temp_int;
+    uint8_t temp_dec;
+    uint8_t checksum;
+} air;
+
+struct Time_parameters_structure {
+    uint8_t time_sec;
+    uint8_t time_min;
+    uint8_t time_hour;
+} time;
 
     // Start communication, transmit I2C Slave address, get result,
     // and Stop communication
+    // 92 temp sensor
+    // 87 time sensor
+
+
+    
+    // humidity int
     ack = twi_start(sla, TWI_WRITE);
+    twi_write(0x00);
     twi_stop();
+
+    ack = twi_start(sla, TWI_READ);
+    air.humid_int = twi_read_nack();
+    twi_stop();
+
+    
+    //humidity decimal
+    ack = twi_start(sla, TWI_WRITE);
+    twi_write(0x01);
+    twi_stop();
+
+    ack = twi_start(sla, TWI_READ);
+    air.humid_dec = twi_read_nack();
+    twi_stop();
+
+    //temp int
+    ack = twi_start(sla, TWI_WRITE);
+    twi_write(0x02);
+    twi_stop();
+
+    ack = twi_start(sla, TWI_READ);
+    air.temp_int = twi_read_nack();
+    twi_stop();
+
+    //temp dec
+    ack = twi_start(sla, TWI_WRITE);
+    twi_write(0x03);
+    twi_stop();
+
+    ack = twi_start(sla, TWI_READ);
+    air.temp_dec = twi_read_nack();
+    twi_stop();
+
+    //checksum
+    ack = twi_start(sla, TWI_WRITE);
+    twi_write(0x04);
+    twi_stop();
+
+    ack = twi_start(sla, TWI_READ);
+    air.checksum = twi_read_nack();
+    twi_stop();
+
+    
+    // zapis
+    
+      uart_puts("Humidity: ");
+      
+      itoa(air.humid_int, string, 10);
+      uart_puts(string);
+      uart_puts(".");
+      
+      itoa(air.humid_dec, string, 10);
+      uart_puts(string);
+      uart_puts(" %\n");
+
+      uart_puts("Temperature: ");
+      itoa(air.temp_int, string, 10);
+      uart_puts(string);
+      uart_puts(".");
+      
+      itoa(air.temp_dec, string, 10);
+      uart_puts(string);
+      uart_puts(" degrees\n");
+
+
+      uart_puts("Checksum: ");
+      itoa(air.checksum, string, 10); 
+      uart_puts(string);
+      uart_puts("\n");
+      uart_puts("\n");
+      uart_puts("\n");
+      
+
+    // time
+    //
+    //
+
+     
+    /*
+    //sec
+    ack = twi_start(sla, TWI_WRITE);
+    twi_write(0x00);
+    twi_stop();
+
+    ack = twi_start(sla, TWI_READ);
+    time.time_sec = twi_read_nack();
+    twi_stop();
+
+    //min
+    ack = twi_start(sla, TWI_WRITE);
+    twi_write(0x01);
+    twi_stop();
+
+    ack = twi_start(sla, TWI_READ);
+    time.time_min = twi_read_nack();
+    twi_stop();
+
+    //hour
+    ack = twi_start(sla, TWI_WRITE);
+    twi_write(0x02);
+    twi_stop();
+
+    ack = twi_start(sla, TWI_READ);
+    time.time_hour = twi_read_nack();
+    twi_stop();
+
+    
+    uart_puts("Time: ");
+      
+    itoa(time.time_hour, string, 10);
+    uart_puts(string);
+    uart_puts(":");
+
+    itoa(time.time_min, string, 10);
+    uart_puts(string);
+    uart_puts(":");
+
+    itoa(time.time_sec, string, 10);
+    uart_puts(string);
+    uart_puts("\n");
+    uart_puts("\n");
+    uart_puts("\n");
+
+     */
 
     // Test ACK/NACK value obtained from I2C bus and send info to UART
     
