2987d7563774c6c13529b85ee236851f41112125
davidhro <david.hromas.00@gmail.com>
2022-11-23 14:15:28 +0100

    comm

--- Project_1/src/main.c
+++ Project_1/src/main.c
@@ -1,29 +1,16 @@
 /***********************************************************************
  * 
- * Stopwatch by Timer/Counter2 on the Liquid Crystal Display (LCD)
- *
+ * Use Analog-to-digital conversion to read push buttons on LCD keypad
+ * shield and display it on LCD screen.
+ * 
  * ATmega328P (Arduino Uno), 16 MHz, PlatformIO
  *
- * Copyright (c) 2017 Tomas Fryza
+ * Copyright (c) 2018 Tomas Fryza
  * Dept. of Radio Electronics, Brno University of Technology, Czechia
  * This work is licensed under the terms of the MIT license.
  * 
- * Components:
- *   16x2 character LCD with parallel interface
- *     VSS  - GND (Power supply ground)
- *     VDD  - +5V (Positive power supply)
- *     Vo   - (Contrast)
- *     RS   - PB0 (Register Select: High for Data transfer, Low for Instruction transfer)
- *     RW   - GND (Read/Write signal: High for Read mode, Low for Write mode)
- *     E    - PB1 (Read/Write Enable: High for Read, falling edge writes data to LCD)
- *     D3:0 - NC (Data bits 3..0, Not Connected)
- *     D4   - PD4 (Data bit 4)
- *     D5   - PD5 (Data bit 5)
- *     D6   - PD6 (Data bit 3)
- *     D7   - PD7 (Data bit 2)
- *     A+K  - Back-light enabled/disabled by PB2
- * 
  **********************************************************************/
+
 
 /* Includes ----------------------------------------------------------*/
 #include <avr/io.h>         // AVR device-specific IO definitions
@@ -37,30 +24,39 @@
 /* Function definitions ----------------------------------------------*/
 /**********************************************************************
  * Function: Main function where the program execution begins
- * Purpose:  Update stopwatch value on LCD screen when 8-bit 
- *           Timer/Counter2 overflows.
+ * Purpose:  Use Timer/Counter1 and start ADC conversion every 100 ms.
+ *           When AD conversion ends, send converted value to LCD screen.
  * Returns:  none
  **********************************************************************/
 int main(void)
 {
     // Initialize display
-    lcd_init(LCD_DISP_ON_CURSOR_BLINK);
+    lcd_init(LCD_DISP_ON);
+    lcd_gotoxy(1, 0); lcd_puts("value:");
+    lcd_gotoxy(3, 1); lcd_puts("key:");
+    lcd_gotoxy(8, 0); lcd_puts("a");  // Put ADC value in decimal
+    lcd_gotoxy(13,0); lcd_puts("b");  // Put ADC value in hexadecimal
+    lcd_gotoxy(8, 1); lcd_puts("c");  // Put button name here
 
-    // Put string(s) on LCD screen
-    
-    lcd_gotoxy(6, 1);
-    lcd_puts("LCD Test");
-    lcd_putc('!');
-     
-    
+    // Configure Analog-to-Digital Convertion unit
+    // Select ADC voltage reference to "AVcc with external capacitor at AREF pin"
+    ADMUX |= (1 << REFS0);
+    ADMUX &= ~(1 << REFS1);
 
-    // Configuration of 8-bit Timer/Counter2 for Stopwatch update
-    // Set the overflow prescaler to 16 ms and enable interrupt
-   TIM2_overflow_16ms();
-   TIM2_overflow_interrupt_enable();
-   
+    // Select input channel ADC0 (voltage divider pin)
+    ADMUX &= ~((1 << MUX0) | (1 << MUX1) | (1 << MUX2) | (1 << MUX3));
+    ADMUX &= ~((0 << MUX0) | (1 << MUX1) | (1 << MUX2) | (1 << MUX3));
+    // Enable ADC module
+    ADCSRA |= (1 << ADEN);
+    // Enable conversion complete interrupt
+    ADCSRA |= (1 << ADIE);
+    // Set clock prescaler to 128
+    ADCSRA |= ((1 << ADPS0) | (1 << ADPS1) | (1 << ADPS2));
 
-
+    // Configure 16-bit Timer/Counter1 to start ADC conversion
+    // Set prescaler to 33 ms and enable overflow interrupt
+    TIM1_overflow_33ms();
+    TIM1_overflow_interrupt_enable();
 
     // Enables interrupts by setting the global interrupt mask
     sei();
@@ -69,7 +65,7 @@ int main(void)
     while (1)
     {
         /* Empty loop. All subsequent operations are performed exclusively 
-         * inside interrupt service routines, ISRs */
+         * inside interrupt service routines ISRs */
     }
 
     // Will never reach this
@@ -79,29 +75,121 @@ int main(void)
 
 /* Interrupt service routines ----------------------------------------*/
 /**********************************************************************
- * Function: Timer/Counter2 overflow interrupt
- * Purpose:  Update the stopwatch on LCD screen every sixth overflow,
- *           ie approximately every 100 ms (6 x 16 ms = 100 ms).
+ * Function: Timer/Counter1 overflow interrupt
+ * Purpose:  Use single conversion mode and start conversion every 100 ms.
  **********************************************************************/
-ISR(TIMER2_OVF_vect)
+ISR(TIMER1_OVF_vect)
 {
-    static uint8_t no_of_overflows = 0;
-    static uint8_t tenths = 1;  // Tenths of a second
-    char string[2];             // String for converted numbers by itoa()
+    // Start ADC conversion
+    ADCSRA |= (1 << ADSC);
+}
 
-    no_of_overflows++;
-    if (no_of_overflows >= 6)
-    {
-        // Do this every 6 x 16 ms = 100 ms
-        no_of_overflows = 0;
+/**********************************************************************
+ * Function: ADC complete interrupt
+ * Purpose:  Display converted value on LCD screen.
+ **********************************************************************/
+ISR(ADC_vect)
+{
+    uint16_t value;
+    char string[4];  // String for converted numbers by itoa()
 
-        // Count tenth of seconds 0, 1, ..., 9, 0, 1, ...
+    // Read converted value
+    // Note that, register pair ADCH and ADCL can be read as a 16-bit value ADC
+    value = ADC;
+    //value = ADC;
+    // Convert "value" to "string" and display it
+
     
-
-        itoa(tenths, string, 10);  // Convert decimal value to string
-        // Display "00:00.tenths"
-        lcd_gotoxy(7, 0);
+    
+    
+    itoa(value, string, 10);
+    if(value > 999)
+    {
+      lcd_gotoxy(8,0);
+      lcd_puts(string);
+    }else{
+      if(value>99)
+      {
+        lcd_gotoxy(9,0);
         lcd_puts(string);
+        lcd_gotoxy(8,0);
+        lcd_putc('0');
+      }else{
+        if(value>9)
+        {
+          lcd_gotoxy(10,0);
+          lcd_puts(string);
+          lcd_gotoxy(8,0);
+          lcd_putc('0');
+          lcd_gotoxy(9,0);
+          lcd_putc('0');
+        }else{
+          lcd_gotoxy(11,0);
+          lcd_puts(string);
+          lcd_gotoxy(8,0);
+          lcd_putc('0');
+          lcd_gotoxy(9,0);
+          lcd_putc('0');
+          lcd_gotoxy(10,0);
+          lcd_putc('0');
+        }
+      }
     }
-    // Else do nothing and exit the ISR
+    if(value > 255)
+    {
+      itoa(value, string, 16);
+      lcd_gotoxy(13,0);
+      lcd_puts(string);
+    }else{
+      if(value > 15)
+      {
+        itoa(value, string, 16);
+        lcd_gotoxy(14,0);
+        lcd_puts(string);
+        lcd_gotoxy(13,0);
+        lcd_putc('0');
+      }else{
+        itoa(value, string, 16);
+        lcd_gotoxy(15,0);
+        lcd_puts(string);
+        lcd_gotoxy(13,0);
+        lcd_putc('0');
+        lcd_gotoxy(14,0);
+        lcd_putc('0');
+      }
+    }
+
+    if(value < 25)
+    {
+      lcd_gotoxy(8,1);
+      lcd_puts("right ");
+    }else{
+      if(value < 125)
+      {
+        lcd_gotoxy(8,1);
+        lcd_puts("up    ");
+      }else{
+        if(value < 280)
+        {
+          lcd_gotoxy(8,1);
+          lcd_puts("down  ");
+        }else{
+          if(value < 450)
+          {
+            lcd_gotoxy(8,1);
+            lcd_puts("left  ");
+          }else{
+            if(value < 670)
+            {
+              lcd_gotoxy(8,1);
+              lcd_puts("select");
+            }else{
+              lcd_gotoxy(8,1);
+              lcd_puts("none  ");
+            }
+          }
+        }
+      }
+    }
+    
 }
